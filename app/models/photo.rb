248a66a672a1069a89abefa287ca3ac77dbb1569
Taka41802 <p280132a@gmail.com>
2025-01-08 15:16:26 +0900

    フォトテーブル作成

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,2 +1,12 @@
 class Photo < ApplicationRecord
+  belongs_to :album
+  belongs_to :user
+
+  before_create :set_posted_date
+
+  private
+
+  def set_posted_date
+    self.posted_date = Date.today
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2025_01_08_025136) do
+ActiveRecord::Schema[7.0].define(version: 2025_01_08_054032) do
   create_table "active_storage_attachments", charset: "utf8mb3", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -46,6 +46,17 @@ ActiveRecord::Schema[7.0].define(version: 2025_01_08_025136) do
     t.integer "user_id"
   end
 
+  create_table "photos", charset: "utf8mb3", force: :cascade do |t|
+    t.text "description"
+    t.date "posted_date", null: false
+    t.bigint "album_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["album_id"], name: "index_photos_on_album_id"
+    t.index ["user_id"], name: "index_photos_on_user_id"
+  end
+
   create_table "users", charset: "utf8mb3", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -61,4 +72,6 @@ ActiveRecord::Schema[7.0].define(version: 2025_01_08_025136) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "photos", "albums"
+  add_foreign_key "photos", "users"
 end
