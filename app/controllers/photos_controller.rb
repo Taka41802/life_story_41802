010deabd1c558bbc75f5f366b4e770c5d8cc2dcb
Taka41802 <p280132a@gmail.com>
2025-01-09 11:34:31 +0900

    ビューの作成

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,14 +1,14 @@
 class PhotosController < ApplicationController
+  before_action :set_album, only: [:new, :create]
 
-  def index
-    @photos = Photo.includes(:user)
+  def new
+    @photo = @album.photos.new
   end
 
   def create
-    @photo = Photo.new(photo_params)
-
+    @photo = @album.photos.new(photo_params)
     if @photo.save
-      redirect_to @photo, notice: 'Photo was successfully created.'
+      redirect_to album_photo_path(@album, @photo), notice: 'Photo was successfully created.'
     else
       render :new
     end
@@ -16,7 +16,11 @@ class PhotosController < ApplicationController
 
   private
 
+  def set_album
+    @album = Album.find(params[:album_id])
+  end
+
   def photo_params
-    params.require(:photo).permit(:title, :description, :album_id, :user_id, :image)
+    params.require(:photo).permit(:title, :description, :album_id, :user_id, :image, :posted_date)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
+
   resources :albums do
-    resources :photos, only: [:new, :create]
+    resources :photos
   end
+
   root to: 'albums#index'
   resources :users, only: :show
 end
