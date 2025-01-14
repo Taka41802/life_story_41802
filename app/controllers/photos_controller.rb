0bdef8e3850d91501bf2a598331841875b2a7b23
Taka41802 <p280132a@gmail.com>
2025-01-14 16:40:01 +0900

    削除機能実装

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,5 +1,6 @@
 class PhotosController < ApplicationController
-  before_action :set_album, only: [:new, :create]
+  before_action :set_album, only: [:new, :create, :edit, :update, :destroy]
+  before_action :set_photo, only: [:edit, :update, :destroy]
 
   def new
     @photo = @album.photos.new
@@ -15,16 +16,41 @@ class PhotosController < ApplicationController
         @photo.image.attach(image)
       end
 
-      redirect_to album_path(@album), notice: 'Photo was successfully created.'
+      redirect_to album_path(@album)
     else
       render :new
     end
+  end
+
+  def edit
+  end
+
+  def update
+    if @photo.update(photo_params.except(:image))
+      images = Array.wrap(photo_params[:image]).reject(&:blank?)
+      @photo.image.purge_later if photo_params[:image].present?
+      images.each do |image|
+        @photo.image.attach(image)
+      end
+
+      redirect_to album_path(@album)
+      render :edit
+    end
+  end
+
+  def destroy
+    @photo.destroy
+    redirect_to album_path(@album)
   end
 
   private
 
   def set_album
     @album = Album.find(params[:album_id])
+  end
+
+  def set_photo
+    @photo = @album.photos.find(params[:id])
   end
 
   def photo_params
