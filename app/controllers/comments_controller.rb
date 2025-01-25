71272712e4ed9ed74a4b2d3cf44fc29e79b4bb27
Taka41802 <p280132a@gmail.com>
2025-01-25 22:21:34 +0900

    コントローラー記入

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,19 @@
 class CommentsController < ApplicationController
+  def create
+    @photo = Photo.find(params[:photo_id])
+    @comment = @photo.comments.build(comment_params)
+    @comment.user = current_user
+    if @comment.save
+      # コメント保存成功時の処理
+      redirect_to photo_path(@photo), notice: 'コメントを投稿しました。'
+    else
+      # コメント保存失敗時の処理
+      render 'photos/show'
+    end
+  end
+
+  private
+    def comment_params
+      params.require(:comment).permit(:body)
+    end
 end
