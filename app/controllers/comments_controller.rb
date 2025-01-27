b09187e4726a59a2201ab13d06ce4ef655d7652a
Taka41802 <p280132a@gmail.com>
2025-01-27 22:30:52 +0900

    コントローラー変更

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,16 +1,17 @@
 class CommentsController < ApplicationController
   def create
-    @photo = Photo.find(params[:photo_id])
+    @album = Album.find(params[:album_id])
+    @photo = @album.photos.find(params[:photo_id])
     @comment = @photo.comments.build(comment_params)
     @comment.user = current_user
+
     if @comment.save
-      # コメント保存成功時の処理
       redirect_to photo_path(@photo), notice: 'コメントを投稿しました。'
     else
-      # コメント保存失敗時の処理
       render 'photos/show'
     end
   end
+end
 
   private
     def comment_params
