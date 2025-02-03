be22b6976e4a4812abbfc1100176491a374c63b7
Taka41802 <p280132a@gmail.com>
2025-02-03 14:11:26 +0900

    ルート追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   resources :albums do
     resources :photos, only: [:show,:new, :create, :edit, :update, :destroy] do
-      resources :comments, only: [:create]
+      resources :comments, only: [:create, :destroy]
     end
   end
 
